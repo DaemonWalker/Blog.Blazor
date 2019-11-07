fefd3d209570d5dea4a1d5129e2920236bc68540
Daemon Walker <215136855@qq.com>
2019-11-07 10:00:02 +0800

    v0.1.1

--- Blog.Blazor/Services/InfoService.cs
+++ Blog.Blazor/Services/InfoService.cs
@@ -1,6 +1,7 @@
 ﻿using MyBlog.Data;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,6 +17,30 @@ namespace Blog.Blazor.Services
         public Task<string> GetDBPath()
         {
             return Task.FromResult(dBContext.GetDBPath());
+        }
+        public Task<List<string>> LS()
+        {
+            return Task.Run(() =>
+            {
+                var dir = new DirectoryInfo(Directory.GetCurrentDirectory());
+                var stack = new Stack<DirectoryInfo>();
+                var result = new List<string>();
+                stack.Push(dir);
+                while (stack.Count > 0)
+                {
+                    dir = stack.Pop();
+                    foreach (var d in dir.GetDirectories())
+                    {
+                        stack.Push(d);
+                    }
+                    foreach (var f in dir.GetFiles())
+                    {
+                        result.Add(f.FullName);
+                    }
+                }
+                return result.OrderBy(_ => _).ToList();
+            });
+
         }
     }
 }
